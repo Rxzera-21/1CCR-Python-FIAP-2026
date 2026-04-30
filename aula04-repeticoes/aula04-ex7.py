56f4d315cb0b6c868e38f97169e8e775e8f735ac
Rxzera-21 <rafamarinucci2007@gmail.com>
2026-04-29 21:26:29 -0300

    aula04

---
+++ aula04-repeticoes/aula04-ex7.py
@@ -0,0 +1,5 @@
+def nmr_positivo(nmr):
+    while nmr < 0:
+        print("O número deve ser positivo!")
+        nmr = int(input("Digite novamente um número positivo: "))
+
---
+++ aula04-repeticoes/aula04b-for.py
@@ -0,0 +1,13 @@
+#
+#qtd_musics = 29 # DB -> viria do banco de dados
+#for count_music in range(qtd_musics):
+#    print(f'Música {count_music}')
+
+# de 1 até 11, pulando de 2 em 2 com for
+#for i in range(1,12):
+#    print(i)
+
+## LAÇOS ALINHADOS
+for i in range(0,4):
+    for j in range(0,3,2):
+        print(f"i:{i}, j:{j}")
